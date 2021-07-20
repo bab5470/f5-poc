b1a10c4612eb660976601bf58c50d5a9d15a3fcb
Brad Baker <brad.baker@dailyxtra.com>
2021-07-20 09:48:51 -0400

    Changed cis_deploy.yaml to match the one from mdditt2000

--- bigip-ctlr/create-cis-deployment.sh
+++ bigip-ctlr/create-cis-deployment.sh
@@ -1,3 +1,4 @@
+kubectl create secret generic bigip-login -n kube-system --from-literal=username=admin --from-literal=password=replace-me-with-your-password
 kubectl create -f customresourcedefinitions.yaml
 kubectl create -f default-ingress-class.yaml
 kubectl create -f k8s-rbac.yaml
